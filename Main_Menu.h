0a8f70771cd3fbed3d3ee001387b9d82a9641931
ranggha <adytiaranggha27@gmail.com>
2017-03-29 19:29:46 +0700

    TUBES ASD / Ranggha-Ery

---
+++ Main_Menu.h
@@ -0,0 +1,16 @@
+#ifndef PROGRAM_H_INCLUDED
+#define PROGRAM_H_INCLUDED
+#include <iostream>
+using namespace std;
+//RANGGHA ADYTIA
+
+void menu();
+void insertParents_Peminjam();
+void insertAscendingParent_Peminjam();
+void viewParent_Peminjam();
+void deleteParent_Peminjam();
+void FindParent_Peminjam();
+void insertChild_Buku();
+void viewChilds_Buku();
+
+#endif // PROGRAM_H_INCLUDED
